28916fdff1e0c30727e4578847dedb231abb324f
RyanRMurray <RyanRMurray@Hotmail.co.uk>
2022-12-01 07:29:11 +0000

    day 01 solution

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,26 +1,62 @@
-use crate::utils::solver_types::{solve_linear, SolutionLinear};
-use anyhow::{anyhow, Result};
+use crate::utils::solver_types::{solve_simultaneous, SolutionSimultaneous};
+use anyhow::Result;
+use itertools::Itertools;
 
 /// Not yet implementd
 pub struct Day1Solution {}
 
 pub fn day01(input: &str) -> Result<f32> {
-    solve_linear::<Day1Solution, _, _, _>(input)
+    solve_simultaneous::<Day1Solution, _, _, _>(input)
 }
 
-impl SolutionLinear<String, String, String> for Day1Solution {
-    fn load(_input: &str) -> Result<String> {
-        Err(anyhow!("load not yet implemented"))
+impl SolutionSimultaneous<Vec<Vec<u32>>, u32, u32> for Day1Solution {
+    fn load(input: &str) -> Result<Vec<Vec<u32>>> {
+        Ok(input
+            .split("\n\n")
+            .map(|chunk| chunk.lines().map(|l| l.parse().unwrap()).collect())
+            .collect())
     }
 
-    fn part1(_input: &mut String) -> Result<String> {
-        Err(anyhow!("part 1 not yet implemented"))
-    }
+    fn solve(input: Vec<Vec<u32>>) -> Result<(u32, u32)> {
+        let sorted = input
+            .iter()
+            .map(|v| v.iter().sum())
+            .sorted()
+            .rev()
+            .collect::<Vec<_>>();
 
-    fn part2(_input: &mut String, _part_1_solution: String) -> Result<String> {
-        Err(anyhow!("part 2 not yet implemented"))
+        Ok((sorted[0], sorted[0] + sorted[1] + sorted[2]))
     }
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::{solutions::day01::Day1Solution, utils::solver_types::SolutionSimultaneous};
+
+    #[test]
+    fn test_answer() {
+        // note: ugly format so we don't lead with a \n
+        let input = r#"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"#;
+
+        let loaded = Day1Solution::load(input).unwrap();
+        let (p1, p2) = Day1Solution::solve(loaded).unwrap();
+        let p1_expected = 24000;
+        let p2_expected = 45000;
+
+        assert_eq!(p1_expected, p1);
+        assert_eq!(p2_expected, p2);
+    }
+}
